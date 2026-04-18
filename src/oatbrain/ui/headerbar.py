a559ed1d56fdc955e98baddd92f597bf7393c70a
Igor Petruk <ipetruk@google.com>
2026-04-17 20:50:14 -0400

    docs(spec): move terminal toggle to right grouping; feat(ui): align header layout with updated spec

--- src/oatbrain/ui/headerbar.py
+++ src/oatbrain/ui/headerbar.py
@@ -7,15 +7,15 @@ class HeaderBar:
 
     def __init__(self, event_bus: EventBus) -> None:
         self.widget = Adw.HeaderBar()
-        
-        # Left: hamburger, tree toggle, terminal toggle, new note
+
+        # Left: hamburger, tree toggle, new note
         self._hamburger_btn = Gtk.MenuButton(icon_name="open-menu-symbolic")
         self._hamburger_btn.set_tooltip_text("Menu")
 
         self.tree_toggle = Gtk.ToggleButton(icon_name="sidebar-show-symbolic")
         self.tree_toggle.set_active(True)
         self.tree_toggle.set_tooltip_text("Toggle File Tree (Ctrl+B)")
-        
+
         self.terminal_toggle = Gtk.ToggleButton(
             icon_name="utilities-terminal-symbolic"
         )
@@ -28,22 +28,26 @@ class HeaderBar:
         left_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
         left_box.append(self._hamburger_btn)
         left_box.append(self.tree_toggle)
-        left_box.append(self.terminal_toggle)
         left_box.append(self._new_note_btn)
         self.widget.pack_start(left_box)
 
         # Center: title/filename
-        self._title_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
+        self._title_box = Gtk.Box(
+            orientation=Gtk.Orientation.HORIZONTAL,
+            spacing=6
+        )
         self._title_box.set_halign(Gtk.Align.CENTER)
-        
+
         self._title_label = Gtk.Label(label="oatbrain")
         self._title_label.add_css_class("title")
-        
+
         self._unsaved_dot = Gtk.Label(label="●")
         self._unsaved_dot.set_visible(False)
         self._unsaved_dot.set_tooltip_text("Unsaved changes")
 
-        self._readonly_lock = Gtk.Image.new_from_icon_name("changes-prevent-symbolic")
+        self._readonly_lock = Gtk.Image.new_from_icon_name(
+            "changes-prevent-symbolic"
+        )
         self._readonly_lock.set_visible(False)
         self._readonly_lock.set_tooltip_text("Read-only")
 
@@ -52,11 +56,15 @@ class HeaderBar:
         self._title_box.append(self._readonly_lock)
         self.widget.set_title_widget(self._title_box)
 
-        # Right: theme switcher
+        # Right: theme switcher, terminal toggle
         self._theme_btn = Gtk.Button(icon_name="display-brightness-symbolic")
         self._theme_btn.set_tooltip_text("Switch Theme")
-        
-        self.widget.pack_end(self._theme_btn)
+
+        right_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
+        right_box.append(self._theme_btn)
+        right_box.append(self.terminal_toggle)
+
+        self.widget.pack_end(right_box)
 
         event_bus.subscribe(StateUpdated, self._on_state_updated)
 
